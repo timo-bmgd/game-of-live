43ce580bc5843a13ef0e65f90306dd83323a2711
timo-bmgd <tynoz@web.de>
2024-11-07 15:19:40 +0100

    fixed errors and added color functionality

--- grid.py
+++ grid.py
@@ -1,11 +1,17 @@
 import pygame
+import numpy as np
 
 class Grid():
 
-    def __init__(self,width:int, height:int, tilesize = 10):
+    def __init__(self,width:int, height:int, tilesize = 2, spawn_rule = [3], kill_rule = [0,1,4,5,6,7,8,9], mono_color = False):
         self.w, self.h = (int)(width/tilesize), (int)(height/tilesize)
         self.tilesize = tilesize
         self.grid = [[0 for x in range(self.w)] for y in range(self.h)] 
+        self.colors = [[(255,255,255) for x in range(self.w)] for y in range(self.h)] 
+        self.spawn_rule = spawn_rule
+        self.kill_rule = kill_rule
+        self.color = (100,200,255)
+        self.mono_color = mono_color
     
     def add(self, x:int, y:int):
         self.grid[x][y] = 1
@@ -32,14 +38,20 @@ class Grid():
         for x in range(self.w):
             for y in range(self.h):
                 new_rect = pygame.Rect(x*self.tilesize, y*self.tilesize, self.tilesize, self.tilesize)
-                if self.grid[x][y] == 1:
-                    pygame.draw.rect(rect=new_rect, color=(255, 255, 255), surface=screen)
+                if self.mono_color:
+                    if self.grid[x][y] == 1:
+                        pygame.draw.rect(rect=new_rect, color=self.color, surface=screen)
+                    else:
+                        pygame.draw.rect(rect=new_rect, color=(0,0,0), surface=screen)
                 else:
-                    pygame.draw.rect(rect=new_rect, color=(0,0,0), surface=screen)
-    
+                    if self.grid[x][y] == 1:
+                        pygame.draw.rect(rect=new_rect, color=self.colors[x][y], surface=screen)
+                    else:
+                        pygame.draw.rect(rect=new_rect, color=(0,0,0), surface=screen)
+
     def on_click(self, mouse_pos):
-        tile_x = (int)(mouse_pos[0]/10)
-        tile_y = (int)(mouse_pos[1]/10)
+        tile_x = (int)(mouse_pos[0]/self.tilesize)
+        tile_y = (int)(mouse_pos[1]/self.tilesize)
         self.switch(tile_x,tile_y)
 
 
@@ -60,16 +72,29 @@ class Grid():
                 if i == 0 and j == 0:
                     continue
                 count += self.is_alive(x+i, y+j)
+                # if self.is_alive(x, y) == 1:
+                    # print(f"{count} Total; counted {self.is_alive(x+i, y+i)} at {x+i} {y+i}")
         return count
 
 
     def iterate(self):
-        for x in self.grid:
-            for y in x:
+        old_grid = self.grid.copy()
+
+        for row in enumerate(self.grid):
+            for col in enumerate(row[1]):
+                x = row[0]
+                y = col[0]
                 n = self.count_neighbors(x, y)
                 if self.is_alive(x, y):
-                    if n < 2 or n > 3:
+                    if n in self.kill_rule:
                         self.grid[x][y] = 0
-                    if n == 2 or n == 3:
+                    else:
+                        self.colors[x][y] = np.array(self.colors[x][y]) * np.array((0.7,0.8,0.9)) + np.array((20,20,20))
+                else:
+                    if n in self.spawn_rule:
                         self.grid[x][y] = 1
+                        self.colors[x][y] =  (255,255,255)
+
+
+        self.grid = old_grid.copy()
         
--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from grid import Grid
 pygame.init()
 width, height = 500, 500
 grid = Grid(500,500)
-grid.checkerboard()
+# grid.checkerboard()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 start_simulation = False
 
 pixels = pygame.PixelArray(screen)
+
+
 
 while running:
     # poll for events
@@ -21,27 +23,24 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
-            print("Mouse was pressed")
             mouse_pos = pygame.mouse.get_pos()
-            print(mouse_pos)
             grid.on_click(mouse_pos)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print("Space was pressed")
                 start_simulation = not start_simulation
+            if event.key == pygame.K_c:
+                grid.checkerboard()
+            # if event.key == pygame.K_r:
+            #     grid.grid = np.zeros((500,500))
+            # if event.key == pygame.K_s:
+            #     grid.grid = np.random.choice([0,1], size=(500,500), p=[0.9,0.1]).tolist()
     
-    # RENDER YOUR GAME HERE
     if(start_simulation):
         grid.iterate()
-
-    # get mouse position
-    
-    # flip() the display to put your work on screen
+        pygame.time.wait(10)    
 
     grid.draw(screen)
-
     pygame.display.flip()    
-
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)
 
 pygame.quit()
